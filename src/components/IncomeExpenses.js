7f456cf3a2fe731f258c8476f830b7d19616498b
keerthu1991 <keerthanambika03@gmail.com>
2020-11-03 23:37:16 +0530

    change

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,6 +1,6 @@
 import React,{useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState'
-import {Transaction} from'./Transaction'
+
 export const IncomeExpenses = () => {
     const {transactions}=useContext(GlobalContext);
     const amounts = transactions.map(transaction => transaction.amount);
