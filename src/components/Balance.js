f7b8593c3b87463148746ad5204cb09c9df0f2b8
keerthu1991 <keerthanambika03@gmail.com>
2020-11-03 23:30:00 +0530

    changed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,5 @@
 import React,{useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState'
-import {Transaction} from'./Transaction'
 
 export const Balance = () => {
     const {transactions}=useContext(GlobalContext);
